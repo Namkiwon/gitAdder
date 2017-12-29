cf1de6b1cbd459070f5b4014124bf7e6653119ec
namkiwon <jtt36@naver.com>
2017-12-29 16:17:10 +0900

    initailize sum

--- src/git/add.java
+++ src/git/add.java
@@ -5,6 +5,10 @@ public class add {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
+		int num1 = 1;
+		int num2 = 2;
+		sum  = num1 + num2;
+	    System.out.println(sum);
 	}
 
 }
